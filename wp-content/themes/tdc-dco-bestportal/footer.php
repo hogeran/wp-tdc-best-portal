e71b5940e90faef852fa23bb83d14636aaefeaa1
Mikael Holgersson <mikael.git@hogeran.se>
2015-09-15 11:58:20 +0200

    lagt till footer och tema screenshot

--- wp-content/themes/tdc-dco-bestportal/footer.php
+++ wp-content/themes/tdc-dco-bestportal/footer.php
@@ -1,36 +1,26 @@
-</div>
-<footer class="footer-2" role="contentinfo">
-  <div class="footer-logo">
-    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="Logo image">
-  </div>
-    <ul>
-      <li><a href="javascript:void(0)">About</a></li>
-      <li><a href="javascript:void(0)">Contact</a></li>
-      <li><a href="javascript:void(0)">Products</a></li>
-    </ul>
-
-    <div class="footer-secondary-links">
-      <ul>
-        <li><a href="javascript:void(0)">Terms and Conditions</a></li>
-        <li><a href="javascript:void(0)">Privacy Policy</a></li>
-      </ul>
-
-      <ul class="footer-social">
-        <li><a href="javascript:void(0)">
-          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/facebook-logo-circle.png" alt="Facebook">
-          </a></li>
-        <li><a href="javascript:void(0)">
-          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="Twitter">
-        </a></li>
-        <li><a href="javascript:void(0)">
-          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/youtube-logo-circle.png" alt="YouTube">
-        </a></li>
-      </ul>
     </div>
-</footer>
+    <footer class="footer-2" role="contentinfo">
+      <div class="footer-logo">
+        <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="Logo image">
+      </div>
+        <ul>
+          <li><a href="javascript:void(0)">About</a></li>
+          <li><a href="javascript:void(0)">Contact</a></li>
+          <li><a href="javascript:void(0)">Products</a></li>
+        </ul>
+
+        <div class="footer-secondary-links">
+          <ul>
+            <li><a href="javascript:void(0)">&copy; <?php echo date('Y');?> Hogeran</a></li>
+          </ul>
+
+        </div>
+    </footer>
 
 
-    <script src="assets/js/jquery-2.1.4.min.js"></script>
-    <script src="assets/js/functions.js" type="text/javascript"> </script>
+    <script src="<?php bloginfo('template_url');?>/js/jquery-2.1.4.min.js"></script>
+    <script src="<?php bloginfo('template_url');?>/js/functions.js" type="text/javascript"> </script>
+	<?php wp_footer(); ?>
+
   </body>
 </html>
