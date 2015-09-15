a7e956d2990ed3818b55e67e90fbc457b68ad47d
Mikael Holgersson <mikael.git@hogeran.se>
2015-09-15 13:09:55 +0200

    fixat the loop i index.php

--- wp-content/themes/tdc-dco-bestportal/index.php
+++ wp-content/themes/tdc-dco-bestportal/index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-<h1>RUBRIK</h1>
+<div class="flex-boxes">
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+            <a href=<?php the_permalink(); ?> class="flex-box">
+                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_2_dark.png" alt="loggo">
+                    <h1 class="flex-title"><?php the_title(); ?></h1>
+                <p><?php the_excerpt(); ?></p>
+            </a>
+        <?php endwhile; ?>
+    <?php else: ?>
+        <h2>Innehåll saknas</h2>
+    <?php endif; ?>
+</div>
 <?php get_footer(); ?>
