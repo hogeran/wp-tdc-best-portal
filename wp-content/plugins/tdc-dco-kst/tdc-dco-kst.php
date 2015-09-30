b1abd925f3fddcac817810465e7b2ab4292e584c
Mikael Holgersson <mikael.holgersson@tdc.se>
2015-09-30 16:07:21 +0200

    uppdaterat plugin

--- wp-content/plugins/tdc-dco-kst/tdc-dco-kst.php
+++ wp-content/plugins/tdc-dco-kst/tdc-dco-kst.php
@@ -1,5 +1,11 @@
 <?php
 /*
 Plugin Name: TDC DCO KST-beställningar
+Version:     1.0
+Author:      Mikael Holgersson
+Author URI:  http://hogeran.se
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: tdc-dco-kst
 */
 ?>
